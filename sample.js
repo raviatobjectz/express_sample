24c8d407f97adc98b9bc9ba174608d2706addcc2
raviatobjectz <ravi@objectzinc.com>
2016-08-10 20:11:59 -0700

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var ip = require('ip');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! : '+ip.address());
 });
 
 app.listen(3000, function () {
